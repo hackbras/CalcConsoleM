00ff25e54cf9362138f2d45d563b40edce82f54a
hackbras <hackbras@outlook.com>
2017-06-08 07:14:34 -0300

    firt commit

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcM
+{
+    class Program:calculo
+    {
+        static void Main(string[] args)
+        {
+            int b=0;            
+            bool sair = false;
+            string msg = "TABELA DE OPERAÇÕES\n\n + = soma,\n - = subtração,\n * = multiplicacao, \n / = divisão, \n ^ = potencia, \n . = raiz";
+            string operacao;
+
+            while (!sair) {
+                Console.WriteLine();
+                Console.WriteLine(msg);                
+
+                Console.Write("\n digite o primeiro numero:");
+                int a = int.Parse(Console.ReadLine());
+                
+                Console.Write("digite o numero da operação desejada:");
+                operacao = Console.ReadLine();
+
+                var c = new calculo();
+
+                //enquanto a operação for falsa 
+                while (c.valida_operacao(operacao)==false)
+                {
+                    Console.Write("Favor selecionar alguma operação válida:");
+                    operacao = Console.ReadLine();
+                }
+
+                if (operacao!="."){
+                    Console.Write("digite o segundo numero:");
+                    b = int.Parse(Console.ReadLine());
+                }    
+                           
+              
+                
+                    switch (operacao){
+                        case "+":
+                            Console.Write("O resultado é: " + c.soma(a, b));
+                            break;
+                        case "-":
+                            Console.Write("O resultado é: " + c.subtracao(a, b));
+                            break;
+                        case "*":
+                            Console.Write("O resultado é: " + c.multiplicacao(a, b));
+                            break;
+                        case "/":
+                            Console.Write("O resultado é: " + c.divisao(a, b));
+                            break;
+                        case "^":
+                            Console.Write("O resultado é: " + c.potencia(a, b));
+                            break;
+                        case ".":
+                            Console.Write("O resultado é: " + c.raiz(a));
+                            break;
+                }
+               
+                
+                Console.Write("\nDeseja sair? \nDigite y para sim e n para não:");
+                string opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "y":
+                        sair = true;
+                        break;
+
+                    default:
+                        sair = false;
+                        break;
+                }
+                Console.WriteLine("\n\n");
+
+                Console.Clear();
+            }
+            
+        }
+
+
+    }
+}
+
---
+++ calculo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcM
+{
+    public class calculo
+    {
+
+        public string[] operacao_opcoes = new string[6] { "+", "-", "*", "/" ,"^" , "."};
+
+        public int soma(int parcela1, int parcela2){
+            int total;
+            total = parcela1 + parcela2;
+            return total;
+        }
+
+        public int subtracao(int minuendo, int subtraendo){
+            int resto;
+            resto = minuendo - subtraendo;
+            return resto;
+        }
+
+        public int multiplicacao(int multiplicando, int multiplicador){
+            int produto;
+            produto = multiplicando * multiplicador;
+            return produto;
+        }
+
+        public int divisao(int dividendo, int divisor){
+            int quociente;
+            quociente = dividendo / divisor;
+            return quociente;
+        }
+
+        public Double potencia(Double a, double b){
+            Double resultado;
+            resultado = Math.Pow(a,b);
+            return resultado;
+        }
+
+        public Double raiz(double num){
+            Double resultado;
+            resultado = Math.Sqrt(num);
+            return resultado;
+        }
+
+        public bool valida_operacao(string a){
+            if (a == operacao_opcoes[0] || a == operacao_opcoes[1] || a == operacao_opcoes[2] 
+            || a == operacao_opcoes[3] || a == operacao_opcoes[4] || a == operacao_opcoes[5] ){
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+    }
+}
